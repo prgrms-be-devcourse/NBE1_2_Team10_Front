14a114925f51664b140ac42fce48ef0ab98fbadf
shmin <gortmtjdgns0943@gmail.com>
2024-10-21 12:38:37 +0900

    css 작성

--- src/screens/reviewdetail/ReviewDetail.jsx
+++ src/screens/reviewdetail/ReviewDetail.jsx
@@ -1,157 +1,159 @@
 import "./ReviewDetail.css";
 
-export const ReviewDetail = ({ className, ...props }) => {
-  return (
-    <div className={"div " + className}>
-      <div className="sub-container">
-        <div className="heading">Comments </div>
-      </div>
-      <img className="rectangle-513" src="rectangle-5130.svg" />
-      <div className="div2">영화 리뷰 내용 </div>
-      <div className="div3">영화 리뷰 제목 </div>
-      <div className="navbar">
-        <div className="logo">
-          <div className="vector">
-            <img className="vector2" src="vector1.svg" />
-            <img className="icon" src="icon0.svg" />
-          </div>
-          <img className="stream-vibe" src="stream-vibe0.svg" />
+export const ReviewDetail = ({className, ...props}) => {
+    return (
+        <div className="reviewdetailscreen">
+            <div className="reviewdetaildiv">
+                <div className="reviewdetailsub-container">
+                    <div className="reviewdetailheading">Comments</div>
+                </div>
+                <img className="reviewdetailrectangle-513" src="rectangle-5130.svg"/>
+                <div className="reviewdetaildiv2">영화 리뷰 내용</div>
+                <div className="reviewdetaildiv3">영화 리뷰 제목</div>
+                <div className="reviewdetailnavbar">
+                    <div className="reviewdetaillogo">
+                        <div className="reviewdetailvector">
+                            <img className="reviewdetailvector2" src="vector1.svg"/>
+                            <img className="reviewdetailicon" src="icon0.svg"/>
+                        </div>
+                        <img className="reviewdetailstream-vibe" src="stream-vibe0.svg"/>
+                    </div>
+                    <div className="reviewdetailbuttons-container">
+                        <div className="reviewdetaildiv4">로그인</div>
+                        <div className="reviewdetaildiv5">회원가입</div>
+                    </div>
+                </div>
+                <div className="reviewdetailfooter">
+                    <div className="reviewdetailcontainer">
+                        <div className="reviewdetailsub-container2">
+                            <div className="reviewdetailheading2">Home</div>
+                            <div className="reviewdetaillinks-container">
+                                <div className="reviewdetailtext-button">Categories</div>
+                                <div className="reviewdetailtext-button">Devices</div>
+                                <div className="reviewdetailtext-button">Pricing</div>
+                                <div className="reviewdetailtext-button">FAQ</div>
+                            </div>
+                        </div>
+                        <div className="reviewdetailsub-container2">
+                            <div className="reviewdetailheading2">Movies</div>
+                            <div className="reviewdetaillinks-container">
+                                <div className="reviewdetailtext-button">Gernes</div>
+                                <div className="reviewdetailtext-button">Trending</div>
+                                <div className="reviewdetailtext-button">New Release</div>
+                                <div className="reviewdetailtext-button">Popular</div>
+                            </div>
+                        </div>
+                        <div className="reviewdetailsub-container2">
+                            <div className="reviewdetailheading2">Shows</div>
+                            <div className="reviewdetaillinks-container">
+                                <div className="reviewdetailtext-button">Gernes</div>
+                                <div className="reviewdetailtext-button">Trending</div>
+                                <div className="reviewdetailtext-button">New Release</div>
+                                <div className="reviewdetailtext-button">Popular</div>
+                            </div>
+                        </div>
+                        <div className="reviewdetailsub-container2">
+                            <div className="reviewdetailheading2">Support</div>
+                            <div className="reviewdetailtext-button">Contact Us</div>
+                        </div>
+                        <div className="reviewdetailsub-container2">
+                            <div className="reviewdetailheading2">Subscription</div>
+                            <div className="reviewdetaillinks-container">
+                                <div className="reviewdetailtext-button">Plans</div>
+                                <div className="reviewdetailtext-button">Features</div>
+                            </div>
+                        </div>
+                        <div className="reviewdetaillinks-container2">
+                            <div className="reviewdetailheading2">Connect With Us</div>
+                            <div className="reviewdetailbuttons-container2">
+                                <div className="reviewdetailbutton">
+                                    <img className="reviewdetailicon2" src="icon1.svg"/>
+                                </div>
+                                <div className="reviewdetailbutton">
+                                    <img className="reviewdetailicon3" src="icon2.svg"/>
+                                </div>
+                                <div className="reviewdetailbutton">
+                                    <img className="reviewdetailicon4" src="icon3.svg"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="reviewdetailcontainer2">
+                        <div className="reviewdetailline"></div>
+                        <div className="reviewdetailcontainer3">
+                            <div className="reviewdetailtext">@2023 streamvib, All Rights Reserved</div>
+                            <div className="reviewdetailbuttons-container3">
+                                <div className="reviewdetailtext-container">Terms of Use</div>
+                                <div className="reviewdetailline2"></div>
+                                <div className="reviewdetailtext-container">Privacy Policy</div>
+                                <div className="reviewdetailline2"></div>
+                                <div className="reviewdetailtext-container">Cookie Policy</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="reviewdetailrectangle-517"></div>
+                <div className="reviewdetaildiv6">댓글 내용</div>
+                <div className="reviewdetailtext2">작성자 아이디</div>
+                <div className="reviewdetailtext3">댓글 게시일자</div>
+                <img
+                    className="reviewdetailheroicons-solid-hand-thumb-up"
+                    src="heroicons-solid-hand-thumb-up0.svg"
+                />
+                <div className="reviewdetailtext4">45</div>
+                <div className="reviewdetailtext5">답글</div>
+                <div className="reviewdetailrectangle-5172"></div>
+                <div className="reviewdetaildiv7">댓글 내용</div>
+                <div className="reviewdetailtext6">작성자 아이디</div>
+                <div className="reviewdetailtext7">댓글 게시일자</div>
+                <img
+                    className="reviewdetailheroicons-solid-hand-thumb-up2"
+                    src="heroicons-solid-hand-thumb-up1.svg"
+                />
+                <div className="reviewdetailtext8">45</div>
+                <div className="reviewdetailtext9">답글(1)</div>
+                <img
+                    className="reviewdetailheroicons-mini-chevron-up"
+                    src="heroicons-mini-chevron-up0.svg"
+                />
+                <div className="reviewdetailrectangle-5173"></div>
+                <div className="reviewdetaildiv8">댓글 내용</div>
+                <div className="reviewdetailtext10">작성자 아이디</div>
+                <div className="reviewdetailtext11">댓글 게시일자</div>
+                <img
+                    className="reviewdetailheroicons-solid-hand-thumb-up3"
+                    src="heroicons-solid-hand-thumb-up2.svg"
+                />
+                <div className="reviewdetailtext12">45</div>
+                <div className="reviewdetailrectangle-5174"></div>
+                <div className="reviewdetaildiv9">댓글 내용</div>
+                <div className="reviewdetailtext13">작성자 아이디</div>
+                <div className="reviewdetailtext14">댓글 게시일자</div>
+                <img
+                    className="reviewdetailheroicons-solid-hand-thumb-up4"
+                    src="heroicons-solid-hand-thumb-up3.svg"
+                />
+                <div className="reviewdetailtext15">45</div>
+                <div className="reviewdetailtext16">답글(3)</div>
+                <img
+                    className="reviewdetailheroicons-mini-chevron-down"
+                    src="heroicons-mini-chevron-down0.svg"
+                />
+                <div className="reviewdetailline-8"></div>
+                <div className="reviewdetailgroup-6">
+                    <div className="reviewdetailrectangle-514"></div>
+                    <div className="reviewdetaildiv10">등록</div>
+                </div>
+                <div className="reviewdetailtext17">댓글 추가</div>
+                <div className="reviewdetailtext18">작성자 아이디</div>
+                <div className="reviewdetailline-82"></div>
+                <div className="reviewdetailgroup-62">
+                    <div className="reviewdetailrectangle-5142"></div>
+                    <div className="reviewdetaildiv11">등록</div>
+                </div>
+                <div className="reviewdetailtext19">댓글 추가</div>
+                <div className="reviewdetailtext20">작성자 아이디</div>
+            </div>
         </div>
-        <div className="buttons-container">
-          <div className="div4">로그인 </div>
-          <div className="div5">회원가입 </div>
-        </div>
-      </div>
-      <div className="footer">
-        <div className="container">
-          <div className="sub-container2">
-            <div className="heading2">Home </div>
-            <div className="links-container">
-              <div className="text-button">Categories </div>
-              <div className="text-button">Devices </div>
-              <div className="text-button">Pricing </div>
-              <div className="text-button">FAQ </div>
-            </div>
-          </div>
-          <div className="sub-container2">
-            <div className="heading2">Movies </div>
-            <div className="links-container">
-              <div className="text-button">Gernes </div>
-              <div className="text-button">Trending </div>
-              <div className="text-button">New Release </div>
-              <div className="text-button">Popular </div>
-            </div>
-          </div>
-          <div className="sub-container2">
-            <div className="heading2">Shows </div>
-            <div className="links-container">
-              <div className="text-button">Gernes </div>
-              <div className="text-button">Trending </div>
-              <div className="text-button">New Release </div>
-              <div className="text-button">Popular </div>
-            </div>
-          </div>
-          <div className="sub-container2">
-            <div className="heading2">Support </div>
-            <div className="text-button">Contact Us </div>
-          </div>
-          <div className="sub-container2">
-            <div className="heading2">Subscription </div>
-            <div className="links-container">
-              <div className="text-button">Plans </div>
-              <div className="text-button">Features </div>
-            </div>
-          </div>
-          <div className="links-container2">
-            <div className="heading2">Connect With Us </div>
-            <div className="buttons-container2">
-              <div className="button">
-                <img className="icon2" src="icon1.svg" />
-              </div>
-              <div className="button">
-                <img className="icon3" src="icon2.svg" />
-              </div>
-              <div className="button">
-                <img className="icon4" src="icon3.svg" />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="container2">
-          <div className="line"></div>
-          <div className="container3">
-            <div className="text">@2023 streamvib, All Rights Reserved </div>
-            <div className="buttons-container3">
-              <div className="text-container">Terms of Use </div>
-              <div className="line2"></div>
-              <div className="text-container">Privacy Policy </div>
-              <div className="line2"></div>
-              <div className="text-container">Cookie Policy </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="rectangle-517"></div>
-      <div className="div6">댓글 내용 </div>
-      <div className="text2">작성자 아이디 </div>
-      <div className="text3">댓글 게시일자 </div>
-      <img
-        className="heroicons-solid-hand-thumb-up"
-        src="heroicons-solid-hand-thumb-up0.svg"
-      />
-      <div className="text4">45 </div>
-      <div className="text5">답글 </div>
-      <div className="rectangle-5172"></div>
-      <div className="div7">댓글 내용 </div>
-      <div className="text6">작성자 아이디 </div>
-      <div className="text7">댓글 게시일자 </div>
-      <img
-        className="heroicons-solid-hand-thumb-up2"
-        src="heroicons-solid-hand-thumb-up1.svg"
-      />
-      <div className="text8">45 </div>
-      <div className="text9">답글(1) </div>
-      <img
-        className="heroicons-mini-chevron-up"
-        src="heroicons-mini-chevron-up0.svg"
-      />
-      <div className="rectangle-5173"></div>
-      <div className="div8">댓글 내용 </div>
-      <div className="text10">작성자 아이디 </div>
-      <div className="text11">댓글 게시일자 </div>
-      <img
-        className="heroicons-solid-hand-thumb-up3"
-        src="heroicons-solid-hand-thumb-up2.svg"
-      />
-      <div className="text12">45 </div>
-      <div className="rectangle-5174"></div>
-      <div className="div9">댓글 내용 </div>
-      <div className="text13">작성자 아이디 </div>
-      <div className="text14">댓글 게시일자 </div>
-      <img
-        className="heroicons-solid-hand-thumb-up4"
-        src="heroicons-solid-hand-thumb-up3.svg"
-      />
-      <div className="text15">45 </div>
-      <div className="text16">답글(3) </div>
-      <img
-        className="heroicons-mini-chevron-down"
-        src="heroicons-mini-chevron-down0.svg"
-      />
-      <div className="line-8"></div>
-      <div className="group-6">
-        <div className="rectangle-514"></div>
-        <div className="div10">등록 </div>
-      </div>
-      <div className="text17">댓글 추가 </div>
-      <div className="text18">작성자 아이디 </div>
-      <div className="line-82"></div>
-      <div className="group-62">
-        <div className="rectangle-5142"></div>
-        <div className="div11">등록 </div>
-      </div>
-      <div className="text19">댓글 추가 </div>
-      <div className="text20">작성자 아이디 </div>
-    </div>
-  );
+    );
 };
--- src/screens/reviewwrite/ReviewWrite.jsx
+++ src/screens/reviewwrite/ReviewWrite.jsx
@@ -1,98 +1,100 @@
 import "./ReviewWrite.css";
 
-export const ReviewWrite = ({ className, ...props }) => {
-  return (
-    <div className={"div " + className}>
-      <div className="footer">
-        <div className="container">
-          <div className="sub-container">
-            <div className="heading">Home </div>
-            <div className="links-container">
-              <div className="text-button">Categories </div>
-              <div className="text-button">Devices </div>
-              <div className="text-button">Pricing </div>
-              <div className="text-button">FAQ </div>
+export const ReviewWrite = ({className, ...props}) => {
+    return (
+        <div className="reviewwritescreen">
+            <div className="reviewwritediv">
+                <div className="reviewwritefooter">
+                    <div className="reviewwritecontainer">
+                        <div className="reviewwritesub-container">
+                            <div className="reviewwriteheading">Home</div>
+                            <div className="reviewwritelinks-container">
+                                <div className="reviewwritetext-button">Categories</div>
+                                <div className="reviewwritetext-button">Devices</div>
+                                <div className="reviewwritetext-button">Pricing</div>
+                                <div className="reviewwritetext-button">FAQ</div>
+                            </div>
+                        </div>
+                        <div className="reviewwritesub-container">
+                            <div className="reviewwriteheading">Movies</div>
+                            <div className="reviewwritelinks-container">
+                                <div className="reviewwritetext-button">Gernes</div>
+                                <div className="reviewwritetext-button">Trending</div>
+                                <div className="reviewwritetext-button">New Release</div>
+                                <div className="reviewwritetext-button">Popular</div>
+                            </div>
+                        </div>
+                        <div className="reviewwritesub-container">
+                            <div className="reviewwriteheading">Shows</div>
+                            <div className="reviewwritelinks-container">
+                                <div className="reviewwritetext-button">Gernes</div>
+                                <div className="reviewwritetext-button">Trending</div>
+                                <div className="reviewwritetext-button">New Release</div>
+                                <div className="reviewwritetext-button">Popular</div>
+                            </div>
+                        </div>
+                        <div className="reviewwritesub-container">
+                            <div className="reviewwriteheading">Support</div>
+                            <div className="reviewwritetext-button">Contact Us</div>
+                        </div>
+                        <div className="reviewwritesub-container">
+                            <div className="reviewwriteheading">Subscription</div>
+                            <div className="reviewwritelinks-container">
+                                <div className="reviewwritetext-button">Plans</div>
+                                <div className="reviewwritetext-button">Features</div>
+                            </div>
+                        </div>
+                        <div className="reviewwritelinks-container2">
+                            <div className="reviewwriteheading">Connect With Us</div>
+                            <div className="reviewwritebuttons-container">
+                                <div className="reviewwritebutton">
+                                    <img className="reviewwriteicon" src="icon0.svg"/>
+                                </div>
+                                <div className="reviewwritebutton">
+                                    <img className="reviewwriteicon2" src="icon1.svg"/>
+                                </div>
+                                <div className="reviewwritebutton">
+                                    <img className="reviewwriteicon3" src="icon2.svg"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="reviewwritecontainer2">
+                        <div className="reviewwriteline"></div>
+                        <div className="reviewwritecontainer3">
+                            <div className="reviewwritetext">@2023 streamvib, All Rights Reserved</div>
+                            <div className="reviewwritebuttons-container2">
+                                <div className="reviewwritetext-container">Terms of Use</div>
+                                <div className="reviewwriteline2"></div>
+                                <div className="reviewwritetext-container">Privacy Policy</div>
+                                <div className="reviewwriteline2"></div>
+                                <div className="reviewwritetext-container">Cookie Policy</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <img className="reviewwriterectangle-513" src="rectangle-5130.svg"/>
+                <div className="reviewwriteline-3"></div>
+                <div className="reviewwritediv2">리뷰 내용</div>
+                <div className="reviewwritediv3">리뷰 제목</div>
+                <div className="reviewwritediv4">리뷰 작성</div>
+                <div className="reviewwritenavbar">
+                    <div className="reviewwritelogo">
+                        <div className="reviewwritevector">
+                            <img className="reviewwritevector2" src="vector1.svg"/>
+                            <img className="reviewwriteicon4" src="icon3.svg"/>
+                        </div>
+                        <img className="reviewwritestream-vibe" src="stream-vibe0.svg"/>
+                    </div>
+                    <div className="reviewwritebuttons-container3">
+                        <div className="reviewwritediv5">로그인</div>
+                        <div className="reviewwritediv6">회원가입</div>
+                    </div>
+                </div>
+                <div className="reviewwriterectangle-514"></div>
+                <div className="reviewwritediv7">등록</div>
+                <div className="reviewwriteline-4"></div>
             </div>
-          </div>
-          <div className="sub-container">
-            <div className="heading">Movies </div>
-            <div className="links-container">
-              <div className="text-button">Gernes </div>
-              <div className="text-button">Trending </div>
-              <div className="text-button">New Release </div>
-              <div className="text-button">Popular </div>
-            </div>
-          </div>
-          <div className="sub-container">
-            <div className="heading">Shows </div>
-            <div className="links-container">
-              <div className="text-button">Gernes </div>
-              <div className="text-button">Trending </div>
-              <div className="text-button">New Release </div>
-              <div className="text-button">Popular </div>
-            </div>
-          </div>
-          <div className="sub-container">
-            <div className="heading">Support </div>
-            <div className="text-button">Contact Us </div>
-          </div>
-          <div className="sub-container">
-            <div className="heading">Subscription </div>
-            <div className="links-container">
-              <div className="text-button">Plans </div>
-              <div className="text-button">Features </div>
-            </div>
-          </div>
-          <div className="links-container2">
-            <div className="heading">Connect With Us </div>
-            <div className="buttons-container">
-              <div className="button">
-                <img className="icon" src="icon0.svg" />
-              </div>
-              <div className="button">
-                <img className="icon2" src="icon1.svg" />
-              </div>
-              <div className="button">
-                <img className="icon3" src="icon2.svg" />
-              </div>
-            </div>
-          </div>
         </div>
-        <div className="container2">
-          <div className="line"></div>
-          <div className="container3">
-            <div className="text">@2023 streamvib, All Rights Reserved </div>
-            <div className="buttons-container2">
-              <div className="text-container">Terms of Use </div>
-              <div className="line2"></div>
-              <div className="text-container">Privacy Policy </div>
-              <div className="line2"></div>
-              <div className="text-container">Cookie Policy </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <img className="rectangle-513" src="rectangle-5130.svg" />
-      <div className="line-3"></div>
-      <div className="div2">리뷰 내용 </div>
-      <div className="div3">리뷰 제목 </div>
-      <div className="div4">리뷰 작성 </div>
-      <div className="navbar">
-        <div className="logo">
-          <div className="vector">
-            <img className="vector2" src="vector1.svg" />
-            <img className="icon4" src="icon3.svg" />
-          </div>
-          <img className="stream-vibe" src="stream-vibe0.svg" />
-        </div>
-        <div className="buttons-container3">
-          <div className="div5">로그인 </div>
-          <div className="div6">회원가입 </div>
-        </div>
-      </div>
-      <div className="rectangle-514"></div>
-      <div className="div7">등록 </div>
-      <div className="line-4"></div>
-    </div>
-  );
+    );
 };
