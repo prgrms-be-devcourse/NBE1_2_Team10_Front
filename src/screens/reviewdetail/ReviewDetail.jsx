07afa2c6153dd52bb1ab1ba08904e3a97e859eac
seonghoon <gortmtjdgns0943@gmail.com>
2024-10-28 15:23:15 +0900

    [리뷰 상세 페이지 수정] - 로고 클릭 시 메인 페이지로 이동 기능 구현 - 로그인/내정보, 회원가입/로그아웃 버튼 구현 - css 수정 및 comment 관련 내용 제거

--- src/screens/reviewdetail/ReviewDetail.jsx
+++ src/screens/reviewdetail/ReviewDetail.jsx
@@ -1,17 +1,43 @@
 import "./ReviewDetail.css";
+import { useNavigate } from 'react-router-dom';
+import React from "react"; // 페이지 이동을 위한 useNavigate 사용
 
 export const ReviewDetail = ({className, ...props}) => {
+
+    const accessToken = localStorage.getItem('accessToken'); // accessToken을 localStorage에서 가져옴
+    const navigate = useNavigate(); // 페이지 이동을 위한 navigate 사용
+
+    const handleLogin = () => {
+        navigate('/signin'); // 로그인 페이지로 이동
+    };
+
+    const handleRegister = () => {
+        navigate('/signup'); // 회원가입 페이지로 이동
+    };
+
+    const handleProfile = () => {
+        navigate('/mypage'); // 내 정보 페이지로 이동
+    };
+
+    const handleLogout = () => {
+        localStorage.removeItem('accessToken'); // accessToken 삭제
+    };
+
+    // 메인 화면으로 이동
+    const handleLogoClick = () => {
+        navigate("/");
+    };
     return (
         <div className="reviewdetailscreen">
             <div className="reviewdetaildiv">
                 <div className="reviewdetailsub-container">
-                    <div className="reviewdetailheading">Comments</div>
+                    {/*<div className="reviewdetailheading">Comments</div>*/}
                 </div>
-                <img className="reviewdetailrectangle-513" src="rectangle-5130.svg"/>
+                {/*<img className="reviewdetailrectangle-513" src="rectangle-5130.svg"/>*/}
                 <div className="reviewdetaildiv2">영화 리뷰 내용</div>
                 <div className="reviewdetaildiv3">영화 리뷰 제목</div>
                 <div className="reviewdetailnavbar">
-                    <div className="reviewdetaillogo">
+                    <div className="reviewdetaillogo" onClick={handleLogoClick}>
                         <div className="reviewdetailvector">
                             <img className="reviewdetailvector2" src="vector1.svg"/>
                             <img className="reviewdetailicon" src="icon0.svg"/>
@@ -19,9 +45,26 @@ export const ReviewDetail = ({className, ...props}) => {
                         <img className="reviewdetailstream-vibe" src="stream-vibe0.svg"/>
                     </div>
                     <div className="reviewdetailbuttons-container">
-                        <div className="reviewdetaildiv4">로그인</div>
-                        <div className="reviewdetaildiv5">회원가입</div>
-                    </div>
+                            {accessToken ? (
+                                <>
+                                    <div className="reviewdetaildiv4" onClick={handleProfile}>
+                                        내 정보
+                                    </div>
+                                    <div className="reviewdetaildiv5" onClick={handleLogout}>
+                                        로그아웃
+                                    </div>
+                                </>
+                            ) : (
+                                <>
+                                    <div className="reviewdetaildiv4" onClick={handleLogin}>
+                                        로그인
+                                    </div>
+                                    <div className="detaildiv5" onClick={handleRegister}>
+                                        회원가입
+                                    </div>
+                                </>
+                            )}
+                        </div>
                 </div>
                 <div className="reviewdetailfooter">
                     <div className="reviewdetailcontainer">
@@ -92,67 +135,67 @@ export const ReviewDetail = ({className, ...props}) => {
                         </div>
                     </div>
                 </div>
-                <div className="reviewdetailrectangle-517"></div>
-                <div className="reviewdetaildiv6">댓글 내용</div>
-                <div className="reviewdetailtext2">작성자 아이디</div>
-                <div className="reviewdetailtext3">댓글 게시일자</div>
-                <img
-                    className="reviewdetailheroicons-solid-hand-thumb-up"
-                    src="heroicons-solid-hand-thumb-up0.svg"
-                />
-                <div className="reviewdetailtext4">45</div>
-                <div className="reviewdetailtext5">답글</div>
-                <div className="reviewdetailrectangle-5172"></div>
-                <div className="reviewdetaildiv7">댓글 내용</div>
-                <div className="reviewdetailtext6">작성자 아이디</div>
-                <div className="reviewdetailtext7">댓글 게시일자</div>
-                <img
-                    className="reviewdetailheroicons-solid-hand-thumb-up2"
-                    src="heroicons-solid-hand-thumb-up1.svg"
-                />
-                <div className="reviewdetailtext8">45</div>
-                <div className="reviewdetailtext9">답글(1)</div>
-                <img
-                    className="reviewdetailheroicons-mini-chevron-up"
-                    src="heroicons-mini-chevron-up0.svg"
-                />
-                <div className="reviewdetailrectangle-5173"></div>
-                <div className="reviewdetaildiv8">댓글 내용</div>
-                <div className="reviewdetailtext10">작성자 아이디</div>
-                <div className="reviewdetailtext11">댓글 게시일자</div>
-                <img
-                    className="reviewdetailheroicons-solid-hand-thumb-up3"
-                    src="heroicons-solid-hand-thumb-up2.svg"
-                />
-                <div className="reviewdetailtext12">45</div>
-                <div className="reviewdetailrectangle-5174"></div>
-                <div className="reviewdetaildiv9">댓글 내용</div>
-                <div className="reviewdetailtext13">작성자 아이디</div>
-                <div className="reviewdetailtext14">댓글 게시일자</div>
-                <img
-                    className="reviewdetailheroicons-solid-hand-thumb-up4"
-                    src="heroicons-solid-hand-thumb-up3.svg"
-                />
-                <div className="reviewdetailtext15">45</div>
-                <div className="reviewdetailtext16">답글(3)</div>
-                <img
-                    className="reviewdetailheroicons-mini-chevron-down"
-                    src="heroicons-mini-chevron-down0.svg"
-                />
-                <div className="reviewdetailline-8"></div>
-                <div className="reviewdetailgroup-6">
-                    <div className="reviewdetailrectangle-514"></div>
-                    <div className="reviewdetaildiv10">등록</div>
-                </div>
-                <div className="reviewdetailtext17">댓글 추가</div>
-                <div className="reviewdetailtext18">작성자 아이디</div>
-                <div className="reviewdetailline-82"></div>
-                <div className="reviewdetailgroup-62">
-                    <div className="reviewdetailrectangle-5142"></div>
-                    <div className="reviewdetaildiv11">등록</div>
-                </div>
-                <div className="reviewdetailtext19">댓글 추가</div>
-                <div className="reviewdetailtext20">작성자 아이디</div>
+                {/*<div className="reviewdetailrectangle-517"></div>*/}
+                {/*<div className="reviewdetaildiv6">댓글 내용</div>*/}
+                {/*<div className="reviewdetailtext2">작성자 아이디</div>*/}
+                {/*<div className="reviewdetailtext3">댓글 게시일자</div>*/}
+                {/*<img*/}
+                {/*    className="reviewdetailheroicons-solid-hand-thumb-up"*/}
+                {/*    src="heroicons-solid-hand-thumb-up0.svg"*/}
+                {/*/>*/}
+                {/*<div className="reviewdetailtext4">45</div>*/}
+                {/*<div className="reviewdetailtext5">답글</div>*/}
+                {/*<div className="reviewdetailrectangle-5172"></div>*/}
+                {/*<div className="reviewdetaildiv7">댓글 내용</div>*/}
+                {/*<div className="reviewdetailtext6">작성자 아이디</div>*/}
+                {/*<div className="reviewdetailtext7">댓글 게시일자</div>*/}
+                {/*<img*/}
+                {/*    className="reviewdetailheroicons-solid-hand-thumb-up2"*/}
+                {/*    src="heroicons-solid-hand-thumb-up1.svg"*/}
+                {/*/>*/}
+                {/*<div className="reviewdetailtext8">45</div>*/}
+                {/*<div className="reviewdetailtext9">답글(1)</div>*/}
+                {/*<img*/}
+                {/*    className="reviewdetailheroicons-mini-chevron-up"*/}
+                {/*    src="heroicons-mini-chevron-up0.svg"*/}
+                {/*/>*/}
+                {/*<div className="reviewdetailrectangle-5173"></div>*/}
+                {/*<div className="reviewdetaildiv8">댓글 내용</div>*/}
+                {/*<div className="reviewdetailtext10">작성자 아이디</div>*/}
+                {/*<div className="reviewdetailtext11">댓글 게시일자</div>*/}
+                {/*<img*/}
+                {/*    className="reviewdetailheroicons-solid-hand-thumb-up3"*/}
+                {/*    src="heroicons-solid-hand-thumb-up2.svg"*/}
+                {/*/>*/}
+                {/*<div className="reviewdetailtext12">45</div>*/}
+                {/*<div className="reviewdetailrectangle-5174"></div>*/}
+                {/*<div className="reviewdetaildiv9">댓글 내용</div>*/}
+                {/*<div className="reviewdetailtext13">작성자 아이디</div>*/}
+                {/*<div className="reviewdetailtext14">댓글 게시일자</div>*/}
+                {/*<img*/}
+                {/*    className="reviewdetailheroicons-solid-hand-thumb-up4"*/}
+                {/*    src="heroicons-solid-hand-thumb-up3.svg"*/}
+                {/*/>*/}
+                {/*<div className="reviewdetailtext15">45</div>*/}
+                {/*<div className="reviewdetailtext16">답글(3)</div>*/}
+                {/*<img*/}
+                {/*    className="reviewdetailheroicons-mini-chevron-down"*/}
+                {/*    src="heroicons-mini-chevron-down0.svg"*/}
+                {/*/>*/}
+                {/*<div className="reviewdetailline-8"></div>*/}
+                {/*<div className="reviewdetailgroup-6">*/}
+                {/*    <div className="reviewdetailrectangle-514"></div>*/}
+                {/*    <div className="reviewdetaildiv10">등록</div>*/}
+                {/*</div>*/}
+                {/*<div className="reviewdetailtext17">댓글 추가</div>*/}
+                {/*<div className="reviewdetailtext18">작성자 아이디</div>*/}
+                {/*<div className="reviewdetailline-82"/>*/}
+                {/*<div className="reviewdetailgroup-62">*/}
+                {/*    <div className="reviewdetailrectangle-5142"></div>*/}
+                {/*    <div className="reviewdetaildiv11">등록</div>*/}
+                {/*</div>*/}
+                {/*<div className="reviewdetailtext19">댓글 추가</div>*/}
+                {/*<div className="reviewdetailtext20">작성자 아이디</div>*/}
             </div>
         </div>
     );
